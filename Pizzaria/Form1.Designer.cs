2caf6f3ea58d209d8a4b972cde2bf5fc44e77d80
Matheus Gonçalves <matheusgoncalves87@hotmail.com>
2020-09-30 23:32:30 -0300

    arquivos

---
+++ Pizzaria/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Pizzaria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSabores = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnImprimir = new System.Windows.Forms.Button();
+            this.cboSabores = new System.Windows.Forms.ComboBox();
+            this.chkCebola = new System.Windows.Forms.CheckBox();
+            this.chkAzeitona = new System.Windows.Forms.CheckBox();
+            this.rdbSemBorda = new System.Windows.Forms.RadioButton();
+            this.rdbComBorda = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblSabores
+            // 
+            this.lblSabores.AutoSize = true;
+            this.lblSabores.Location = new System.Drawing.Point(101, 59);
+            this.lblSabores.Name = "lblSabores";
+            this.lblSabores.Size = new System.Drawing.Size(99, 13);
+            this.lblSabores.TabIndex = 0;
+            this.lblSabores.Text = "Sabores das Pizzas";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chkAzeitona);
+            this.groupBox1.Controls.Add(this.chkCebola);
+            this.groupBox1.Location = new System.Drawing.Point(126, 119);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ingredientes Adicionais";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rdbComBorda);
+            this.groupBox2.Controls.Add(this.rdbSemBorda);
+            this.groupBox2.Location = new System.Drawing.Point(426, 119);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Borda";
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(176, 249);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(113, 23);
+            this.btnLimpar.TabIndex = 3;
+            this.btnLimpar.Text = "Limpar Pedido";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnImprimir
+            // 
+            this.btnImprimir.Location = new System.Drawing.Point(381, 249);
+            this.btnImprimir.Name = "btnImprimir";
+            this.btnImprimir.Size = new System.Drawing.Size(90, 23);
+            this.btnImprimir.TabIndex = 4;
+            this.btnImprimir.Text = "Imprimir Pedido";
+            this.btnImprimir.UseVisualStyleBackColor = true;
+            this.btnImprimir.Click += new System.EventHandler(this.btnImprimir_Click);
+            // 
+            // cboSabores
+            // 
+            this.cboSabores.FormattingEnabled = true;
+            this.cboSabores.Location = new System.Drawing.Point(221, 56);
+            this.cboSabores.Name = "cboSabores";
+            this.cboSabores.Size = new System.Drawing.Size(121, 21);
+            this.cboSabores.TabIndex = 5;
+            // 
+            // chkCebola
+            // 
+            this.chkCebola.AutoSize = true;
+            this.chkCebola.Location = new System.Drawing.Point(22, 30);
+            this.chkCebola.Name = "chkCebola";
+            this.chkCebola.Size = new System.Drawing.Size(59, 17);
+            this.chkCebola.TabIndex = 0;
+            this.chkCebola.Text = "Cebola";
+            this.chkCebola.UseVisualStyleBackColor = true;
+            // 
+            // chkAzeitona
+            // 
+            this.chkAzeitona.AutoSize = true;
+            this.chkAzeitona.Location = new System.Drawing.Point(22, 53);
+            this.chkAzeitona.Name = "chkAzeitona";
+            this.chkAzeitona.Size = new System.Drawing.Size(67, 17);
+            this.chkAzeitona.TabIndex = 1;
+            this.chkAzeitona.Text = "Azeitona";
+            this.chkAzeitona.UseVisualStyleBackColor = true;
+            // 
+            // rdbSemBorda
+            // 
+            this.rdbSemBorda.AutoSize = true;
+            this.rdbSemBorda.Location = new System.Drawing.Point(50, 30);
+            this.rdbSemBorda.Name = "rdbSemBorda";
+            this.rdbSemBorda.Size = new System.Drawing.Size(77, 17);
+            this.rdbSemBorda.TabIndex = 0;
+            this.rdbSemBorda.TabStop = true;
+            this.rdbSemBorda.Text = "Sem Borda";
+            this.rdbSemBorda.UseVisualStyleBackColor = true;
+            // 
+            // rdbComBorda
+            // 
+            this.rdbComBorda.AutoSize = true;
+            this.rdbComBorda.Location = new System.Drawing.Point(50, 54);
+            this.rdbComBorda.Name = "rdbComBorda";
+            this.rdbComBorda.Size = new System.Drawing.Size(77, 17);
+            this.rdbComBorda.TabIndex = 1;
+            this.rdbComBorda.TabStop = true;
+            this.rdbComBorda.Text = "Com Borda";
+            this.rdbComBorda.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cboSabores);
+            this.Controls.Add(this.btnImprimir);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblSabores);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSabores;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnImprimir;
+        private System.Windows.Forms.ComboBox cboSabores;
+        private System.Windows.Forms.CheckBox chkAzeitona;
+        private System.Windows.Forms.CheckBox chkCebola;
+        private System.Windows.Forms.RadioButton rdbComBorda;
+        private System.Windows.Forms.RadioButton rdbSemBorda;
+    }
+}
+
